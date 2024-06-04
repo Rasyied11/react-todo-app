f331f346d06763afb12658aac0163e97aec04c03
Rasyied11 <abdulrasyied182@gmail.com>
2024-06-04 12:44:58 +0800

    Develop toggle todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,28 @@ function App() {
     },
   ])
 
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
+
+  // const updatedTodos = todos.map((todo)) => {
+  //   if (todo.id === todoId) {
+  //     todo.completed = !todo.completed
+  //   }
+  // }
+
   console.log(todos)
 
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-      <Todos todos={todos} />
+      <Todos todos={todos} toggleCompleted={toggleCompleted} />
     </div>
   )
 }
